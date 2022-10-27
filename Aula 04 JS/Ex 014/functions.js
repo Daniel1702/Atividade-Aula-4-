de22d80d5f03c77f871c3e4ed492e2e6e844fc21
Daniel1702 <106408440+Daniel1702@users.noreply.github.com>
2022-10-27 19:04:14 -0300

    Add files via upload
    Organizando conteúdos

---
+++ Aula 04 JS/Ex 014/functions.js
@@ -0,0 +1,5 @@
+function info() {
+    let agora = new Date
+    let saida = document.getElementById('saida')
+    saida.innerHTML = `<p>Meu sistema detectou iso: <mark>${agora}</mark></p>` // Texto Editado -- Daniel -- 
+}
